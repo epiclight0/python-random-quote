5b43c241ca6300c08c216039319ae41a9ec02eb6
epiclight0 <pavlo.chikalo999@gmail.com>
2021-07-03 15:08:11 +0300

    Add quotes fix all issues

--- get-quote.py
+++ get-quote.py
@@ -7,7 +7,18 @@ def primary():
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  for i in quotes:
+    print(i,end="")
+  print('\nLast quote: ' + quotes[rnd])
+
+def adddata():
+  str1 = "This quote random-number is: " + str(random.randrange(0, 999))
+  data = ['', str1]
+  f = open("E:/python-random-quote/quotes.txt","a")
+  f.writelines('\n'.join(data))
+  f.close()
 
 if __name__== "__main__":
   primary()
+  adddata()
+
